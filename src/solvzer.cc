d6c2905ff976697f7f267e98f519b95001cea185
Guillaume Marques <marque_o@yaka.epita.fr>
2017-06-11 22:52:59 +0200

    solver: clean main
    * src/solvzer.cc: Here.

--- src/solvzer.cc
+++ src/solvzer.cc
@@ -9,35 +9,21 @@ int main()
 {
   cube::Search search;
 
-  /*std::string init_state =
-    "RUBFUBFDBUDDFRUUBFDLLFFUFRLUBFUDLRRDBRLFLLBLRLBDRBDRDU";
-
-  auto face = cube::Face(init_state);
+  /*auto face = cube::Face(init_state);
 
   std::cout << face << std::endl;
 
   auto solution = search.solution(init_state, DEPTH);
-  std::cout << "Solution: " << solution << std::endl;
 
   if (!search.ack_solution(init_state, solution))
     std::cout << "Solution is wrong" << std::endl; */
 
-  /*
-  std::cout << "============================================================\n";
-
-  std::string moves =
-    "U2 R F2 D B' L B R F' L2 U' R' B' R' F' D2 F' L2 D2 L B F2 U' L F2 R' U2 B2 R2 B2";
-
-  auto new_state = "BRDRUDULURLFBRFDDFFFFBFDBRBLFRUDFBBDRDLBLULLDLUUUBLRRU";
-  auto sol = search.solution(new_state, DEPTH);
-  std::cout << sol << std::endl; */
-
   auto face = cube::Face(cube::Cube::solved_state_);
   std::cout << face << std::endl;
+
   //std::string moves =
-  //  "L' D2 R1 F1 R1 F2 L' F' D' F2 L' F' R2 B' D' F1 D' R1 D' F' R2 B' F2 B2 U2 L1 D1 B2 U' L'";
-  std::string moves = "U2 B2 L' B1 U2 B2 R1 D2 F1 R2 U2 L' F2 D1 R2 L2 U2 D2 U2 L' U' R2 D2 L' R1 D' B1 R2 L' B'";
-  face.scramble(moves);
+  //  "L'D2R1F1R1F2L'F'D'F2L'F'R2B'D'F1D'R1D'F'R2B'F2B2U2L1D1B2U'L'";
+  face.scramble();
 
   auto sol = search.solution(face, DEPTH);
   //assert(search.ack_solution(face, sol));
