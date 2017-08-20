8adf5234e0d84918fe52aa6d2709675d1fb64522
Thibault Liotier <liotie_t@epita.fr>
2017-08-20 08:31:49 +0200

    scripts: add test and run script

---
+++ src/resolve/run-step-motor.sh
@@ -0,0 +1,9 @@
+python step_motor_final.py right 300 17 18
+sleep 2
+python step_motor_final.py right 300 27 22
+sleep 2
+python step_motor_final.py right 300 23 24
+sleep 2
+python step_motor_final.py right 300 6 12
+sleep 2
+python step_motor_final.py right 300 19 16
---
+++ src/run-step-motor.sh
@@ -0,0 +1,24 @@
+python step_motor_final.py left 410 17 18
+sleep 1
+python step_motor_final.py right 400 17 18
+sleep 1
+python step_motor_final.py left 400 27 22
+sleep 1
+python step_motor_final.py right 400 27 22
+sleep 1
+python step_motor_final.py left 400 23 24
+sleep 1
+python step_motor_final.py right 400 23 24
+sleep 1
+python step_motor_final.py left 410 20 21
+sleep 1
+python step_motor_final.py right 400 20 21
+sleep 1
+python step_motor_final.py left 400 19 26
+sleep 1
+python step_motor_final.py right 400 19 26
+sleep 1
+python step_motor_final.py left 400 6 13
+sleep 1
+python step_motor_final.py right 410 6 13
+sleep 1
