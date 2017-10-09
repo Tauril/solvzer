72631995c2a583847e501978c1e03b1acb87477f
Cyprien Orfila <orfila_c@epita.fr>
2017-10-10 01:31:37 +0200

    detection: remove useless functions in cube_detector

--- src/detect/cube_detector.hh
+++ src/detect/cube_detector.hh
@@ -14,11 +14,6 @@ namespace detect
       std::string detect_cube();
 
     private:
-      bool ack_nb_faces();
-      bool pass_sanity_duplicates();
-      bool pass_unknown_colors();
-      bool no_unknown_colors(Detector& d) const;
-
       Detector t1_, t2_, b1_, b2_;
       Displayer displayer_ = Displayer("solvzer");
   };
